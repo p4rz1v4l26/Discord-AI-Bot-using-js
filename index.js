560fc59741cf6a6c7e66f49bf918004e1d721b1e
p4rz1v4l26 <20211cst0067@gmail.com>
2024-02-12 23:55:23 +0530

    Updated

--- index.js
+++ index.js
@@ -9,37 +9,29 @@ const client = new Client({
 client.on('ready',()=>{
     console.log("The BOT is Online.")
 });
-
-const IGNORE_PREFIX = "!";
-const CHANNELS = ['1206594145582448660'];
-
 const openai=new OpenAI({
     apiKey:process.env.OPENAI_KEY,
 })
+const IGNORE_PREFIX = "!";
+const CHANNELS = ['1206594145582448660'];
+const users = new Set();
 
-client.on('messageCreate',async(message)=>{
-    if (message.author.bot) return;
-    if (message.content.startsWith(IGNORE_PREFIX)) return;
-    if(!CHANNELS.includes(message.channelId) && !message.mentions,users.has(client.user.id)) return;
+client.on('messageCreate', async (message) => {
+    if (message.author.bot || message.content.startsWith(IGNORE_PREFIX)) return;
+    if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
+
+    users.add(message.author.id);
 
     const response = await openai.chat.completions.create({
-        model:'gpt-4',
-        messages:[
-            {//name:
-            role: 'system',
-            content:'Chat GPT is a friendly Chatbot'
-            },
-            {//name:
-                role: 'user',
-                content: message.content
-                },    
-            ],
-    })
-    .catch((error)=>console.error("OpenAI error: \n",error));
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role: 'system', content: 'Chat GPT is a friendly Chatbot' },
+            { role: 'user', content: message.content },
+        ],
+    }).catch((error) => console.error("OpenAI error: \n", error));
 
     message.reply(response.choices[0].message.content);
 });
-
 
 
 client.login(process.env.TOKEN);
