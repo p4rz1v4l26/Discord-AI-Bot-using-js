844b1785736d838f22b4f375aaee5fc13f2770ce
p4rz1v4l26 <20211cst0067@gmail.com>
2024-02-13 19:15:29 +0530

    Bugs Fixed

--- index.js
+++ index.js
@@ -22,15 +22,63 @@ client.on('messageCreate', async (message) => {
 
     users.add(message.author.id);
 
+    const sendTypingInterval = setInterval(() => {
+        message.channel.sendTyping();
+    },5000);
+
+    let conversation = [];
+    conversation.push({
+        role:'system',
+        content: 'Chat GPT is a friendly Chatbot.'
+    });
+
+    let prevMessages=await message.channel.messages.fetch({limit:10});
+    prevMessages.reverse();
+
+    prevMessages.forEach((msg)=>{
+        if (msg.author.bot && msg.author.id !== client.user.id) return;// I want my bot to read its own messages thats why
+        if (msg.content.startsWith(IGNORE_PREFIX)) return;
+
+        const username=msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
+
+        if(msg.author.id=== client.user.id){ //ofc its my bot only
+            conversation.push({
+                role:'assistant',
+                name:username,
+                content:msg.content,
+            });
+            return;
+        }
+        conversation.push({
+            role:'user',
+            name:username,
+            content:msg.content,
+        });
+    })
+
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
-        messages: [
-            { role: 'system', content: 'Chat GPT is a friendly Chatbot' },
-            { role: 'user', content: message.content },
-        ],
-    }).catch((error) => console.error("OpenAI error: \n", error));
+        messages: conversation,
+    })
+    .catch((error) => console.error("OpenAI error: \n", error));
 
-    message.reply(response.choices[0].message.content);
+    clearInterval(sendTypingInterval);
+
+    if (!response){
+        message.reply("I'm having some trouble with OpenAI API , ask P4RZ1V4L to look into it.");
+        return;
+    }
+
+    const responseMessage= response.choices[0].message.content;
+    const chunkSizeLimit = 2000;
+    for (let i=0;i<responseMessage.length;i+=chunkSizeLimit){
+        const chunk = responseMessage.substring(i,i+chunkSizeLimit);
+        await message.reply(chunk);
+    }
+
+
+
+
 });
 
 
